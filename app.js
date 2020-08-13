6023c84100360f29f3a7d06eb04df848de0721ef
Vivek Jain <vivekjain172000@gmail.com>
2020-08-14 00:10:58 +0530

    Added a POST Route to add a new Campground

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const request = require("request");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 var campgrounds = [
@@ -30,9 +32,13 @@ app.get("/campgrounds", function (req, res) {
     res.render("campgrounds", { campgrounds: campgrounds });
 });
 
-// app.post("/capmgrounds", function (req, res) {
-
-// });
+app.post("/campgrounds", function (req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = { name: name, image: image }
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
 
 app.get("/campgrounds/new", function (req, res) {
     res.render("new");
