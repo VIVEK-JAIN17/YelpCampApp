1e1ceb900e9cad8b7ae5e22de9cfea8ba786d16e
Vivek Jain <vivekjain172000@gmail.com>
2020-09-03 22:06:49 +0530

    Some Design Changes

--- app.js
+++ app.js
@@ -18,28 +18,9 @@ const Campground = mongoose.model("Campground", campgroundSchema);
 const url = 'mongodb://localhost:27017/YelpCamp';
 const connect = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
-// connect.then((db) => {
-//     console.log('Connected correctlyto server !');
-
-//     Campground.create({
-//         name: "Rishikesh",
-//         image: "https://www.easemytrip.com/travel/img/rishikesh-camping.jpg",
-//         description: "Located in the foothills of Himalayas, Rishikesh is among the best camping destinations in India. The amazing Rishikesh camp is not only close to the lush green nature but also has a more tranquil feel. For people looking for some peace away from the chaos of city life, there are tents here, styled in a hermit fashion and are designed to give you a total detached time. The food served here is completely organic and you can also try activities like rafting, trekking, yoga and spa sessions. Camping in Rishikesh is something you should never miss!"
-
-//     }).then((camp) => {
-//         console.log("Successfully Created Campground", camp);
-//         return Campground.find({});
-
-//     }).then((camps) => {
-//         console.log('Found Some Camps', camps);
-
-//     }).catch((err) => console.log(err));
-
-// }).catch((err) => console.log(err));
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 // Creating several routes for the app
 // 1. A route to the landing page
@@ -92,6 +73,6 @@ app.get("/campground/:id", function (req, res) {
         }).catch((err) => console.log(err));
 })
 
-app.listen(3000, function () {
+app.listen(3001, function () {
     console.log("The Yelp Camp Server is up and running !");
 })
