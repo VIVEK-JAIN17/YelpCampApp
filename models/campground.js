30ef29097458ab4dad1712db79baa4ff01758a46
Vivek Jain <vivekjain172000@gmail.com>
2020-09-04 15:50:31 +0530

    updated Schema

--- models/campground.js
+++ models/campground.js
@@ -1,10 +1,26 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const Comment = require('./comment');
 
 // SCHEMA SETUP
 const campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+}, { timestamps: true });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
