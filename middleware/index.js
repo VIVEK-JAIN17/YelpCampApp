9d952089029bb8b94ba6607df8187a13e5bc26c3
Vivek Jain <vivekjain172000@gmail.com>
2020-10-01 18:40:31 +0530

    Added User Dashboard

--- middleware/index.js
+++ middleware/index.js
@@ -43,3 +43,17 @@ exports.authComment = (req, res, next) => {
         res.redirect("back");
     }
 }
+
+exports.verifyUser = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        if (req.user._id.equals(req.params.id)) {
+            return next();
+        } else {
+            req.flash("error", "You do not have permission to do that !");
+            res.redirect('back');
+        }
+    } else {
+        req.flash("error", "you are not logged in !!");
+        res.redirect("back");
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -25,4 +25,24 @@ router.get('/:id/profile', (req, res) => {
         }).catch((err) => { console.log(err); });
 });
 
+router.get('/:id/dashboard', auth.verifyUser, (req, res) => {
+    User.findById(req.user._id)
+        .then((user) => {
+            if (user != null) {
+                Campground.find({ 'author.id': req.user._id })
+                    .then((campgrounds) => {
+                        if (campgrounds != null) {
+                            res.render("users/dashboard", { user: user, campgrounds: campgrounds });
+                        } else {
+                            req.flash("error", "Something Went Wrong !!");
+                            res.redirect('back');
+                        }
+                    }).catch((err) => { console.log(err); });
+            } else {
+                req.flash("error", "Something Went Wrong !!");
+                res.redirect('back');
+            }
+        }).catch((err) => { console.log(err); });
+});
+
 module.exports = router;
