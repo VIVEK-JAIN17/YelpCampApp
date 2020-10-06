fb62bd7084c3c4653684a7d3a8daf2375dd617a8
Vivek Jain <vivekjain172000@gmail.com>
2020-10-07 01:37:45 +0530

    Added rating in reviews

--- models/comment.js
+++ models/comment.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 
 const commentSchema = new mongoose.Schema({
+    rating: {
+        type: Number,
+        required: true,
+        min: 1, max: 5
+    },
     comment: {
         type: String,
         required: true
